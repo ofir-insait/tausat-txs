8a3c09fa7a54fd35e0045381f120e8191ecaa97f
Ofir Cohen <ofir@insait.io>
2022-07-09 20:56:11 +0300

    Added counters decoding for covering all range between 0-255

--- test_txs_packet_decoder.py
+++ test_txs_packet_decoder.py
@@ -1,7 +1,7 @@
 from txs_packet_decoder import TXSPacketDecoder
 
 
-def test_virtual_channel_decode():
+def test_virtual_channel_decode_all_ones():
     paths = ["packets/idle_packet_0.bin",
              "packets/idle_packet_1.bin",
              "packets/idle_packet_2.bin"]
@@ -28,3 +28,41 @@ def test_virtual_channel_decode():
             packet_data = f.read()
         txs_packet = TXSPacketDecoder(packet_data)
         assert txs_packet.virtual_channel_id == 1
+
+
+def test_virtual_channel_decode_counter_0_to_29():
+    paths = ["packets/tausat2_counter_0_to_29.bin"]
+    for path in paths:
+        with open(path, 'rb') as f:
+            packet_data = f.read()
+        txs_packet = TXSPacketDecoder(packet_data)
+        assert txs_packet.virtual_channel_id == 1
+        assert txs_packet.decoded_payload_bytes[:30] == bytes([0x1]) * 30
+        total_len = 30
+        assert txs_packet.decoded_payload_bytes[30:30 + total_len] == bytes(
+            range(0, 30))
+
+
+def test_virtual_channel_decode_counter_30_to_149():
+    paths = ["packets/tausat2_counter_30_to_149.bin"]
+    for path in paths:
+        with open(path, 'rb') as f:
+            packet_data = f.read()
+        txs_packet = TXSPacketDecoder(packet_data)
+        assert txs_packet.virtual_channel_id == 1
+        assert txs_packet.decoded_payload_bytes[:30] == bytes([0x1]) * 30
+        assert txs_packet.decoded_payload_bytes[30:150] == bytes(
+            range(30, 150))
+
+
+def test_virtual_channel_decode_counter_150_to_255():
+    paths = ["packets/tausat2_counter_150_to_255.bin"]
+    for path in paths:
+        with open(path, 'rb') as f:
+            packet_data = f.read()
+        txs_packet = TXSPacketDecoder(packet_data)
+        assert txs_packet.virtual_channel_id == 1
+        assert txs_packet.decoded_payload_bytes[:30] == bytes([0x1]) * 30
+        total_len = 255 - 150 + 1
+        assert txs_packet.decoded_payload_bytes[30:30 + total_len] == bytes(
+            range(150, 256))
